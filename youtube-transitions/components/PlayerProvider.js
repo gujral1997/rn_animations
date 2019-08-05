dedc7ddfc0de7de349ab5d0af023f4dd66ad3031
Ansh Gujral <anshgujral1997@gmail.com>
2019-08-06 00:34:37 +0530

    add sliding animation
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

--- youtube-transitions/components/PlayerProvider.js
+++ youtube-transitions/components/PlayerProvider.js
@@ -1,12 +1,15 @@
 // @flow
 import * as React from 'react';
 import {
-  View, StyleSheet, Dimensions, StatusBar,
+  View, StyleSheet, Dimensions, StatusBar, Animated, Easing,
 } from 'react-native';
 
+// import { Easing } from 'react-native-reanimated';
 import PlayerContext from './PlayerContext';
 import VideoModal from './VideoModal';
 import { type Video } from './videos';
+
+// const { Animated, Easing } = DangerZone;
 
 const { height } = Dimensions.get('window');
 
@@ -23,14 +26,29 @@ export default class PlayerProvider extends React.PureComponent<PlayerProviderPr
     video: null,
   };
 
+  animation = new Animated.Value(0);
+
+
+  toggleVideo=() => {
+    Animated.timing(this.animation, {
+      duration: 300,
+      toValue: 1,
+      easing: Easing.inOut(Easing.ease),
+    }).start();
+  }
+
   setVideo = (video: Video | null) => {
     this.setState({ video }, this.toggleVideo);
   };
 
   render() {
-    const { setVideo } = this;
+    const { setVideo, animation } = this;
     const { children } = this.props;
     const { video } = this.state;
+    const translateY = animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-height, 0],
+    });
     return (
       <PlayerContext.Provider value={{ video, setVideo }}>
         <StatusBar barStyle="dark-content" />
@@ -38,11 +56,11 @@ export default class PlayerProvider extends React.PureComponent<PlayerProviderPr
           <View style={StyleSheet.absoluteFill}>
             {children}
           </View>
-          <View>
+          <Animated.View style={{ transform: [{ translateY }] }}>
             {
               video && <VideoModal {...{ video }} />
             }
-          </View>
+          </Animated.View>
         </View>
       </PlayerContext.Provider>
     );
